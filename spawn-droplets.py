74f97dd8c53f9f13c49b6eba77bad1341716ded0
Sebastian Bolanos <sebasheston@protonmail.com>
2020-07-06 12:12:53 +0100

    Add docker to host.nix

--- spawn-droplets.py
+++ spawn-droplets.py
@@ -7,12 +7,17 @@ write_files:
 - path: /etc/nixos/host.nix
   permissions: '0644'
   content: |
-    {pkgs, ...}:
+    { config, pkgs, ... }:
     {
-      environment.systemPackages = with pkgs; [ vim ];
+        virtualisation.docker = {
+            enable = true;
+            liveRestore = false;
+            enableOnBoot = true;
+            autoPrune.enable = true;
+        };
     }
 runcmd:
-  - curl https://raw.githubusercontent.com/elitak/nixos-infect/master/nixos-infect | NIXOS_IMPORT=./host.nix NIX_CHANNEL=nixos-20.03 bash 2>&1 | tee /tmp/infect.log
+  - curl https://raw.githubusercontent.com/elitak/nixos-infect/master/nixos-infect | PROVIDER=digitalocean NIXOS_IMPORT=./host.nix NIX_CHANNEL=nixos-20.03 bash 2>&1 | tee /tmp/infect.log
   """
 
 # remember to

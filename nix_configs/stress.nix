ab3cd0e39235426889bbdf0a313efa4b6bf38a3c
Sebastian Bolanos <sebasheston@protonmail.com>
2020-09-09 17:39:03 +0200

    Working nix config deploy

--- nix_configs/stress.nix
+++ nix_configs/stress.nix
@@ -7,44 +7,17 @@ let
       ${appName}:
         image: sebohe/wcstress:latest
         restart: always
+        deploy:
+          replicas: ${scale}
+        environment:
+          STRESS_TEST: "1"
 
       logspout:
-        image: gliderlabs/logspout:v3
-        command: 'udp://logstash:5000'
-        links:
-          - logstash
+        image: gliderlabs/logspout
         volumes:
-          - '/var/run/docker.sock:/tmp/docker.sock'
-        depends_on:
-          - elasticsearch
-          - logstash
-          - kibana
-
-      logstash:
-        image: logstash:2.1.1
-        environment:
-          - STDOUT=true
-        links:
-          - elasticsearch
-        depends_on:
-          - elasticsearch
-          - kibana
-        command: 'logstash -e "input { udp { port => 5000 } } output { elasticsearch { hosts => elasticsearch } }"'
-      kibana:
-        image: kibana:4.1.2
-        links:
-          - elasticsearch
-        environment:
-          - ELASTICSEARCH_URL=http://elasticsearch:9200
-        ports:
-          - '5601:5601'
-        depends_on:
-          - elasticsearch
-      elasticsearch:
-        image: elasticsearch:1.5.2
-        ports:
-          - '9200:9200'
-          - '9300:9300'
+          - /etc/hostname:/etc/host_hostname:ro
+          - /var/run/docker.sock:/var/run/docker.sock
+        command: syslog+tls://logs2.papertrailapp.com:41136
   '';
 in
 {
@@ -55,17 +28,27 @@ in
      enableOnBoot = true;
      autoPrune.enable = true;
   };
-  systemd.services.stress_test = {
-    description = "Stress test docker compose";
+  systemd.services.init-swarm = {
+    description = "Init Docker swarm";
     wantedBy = [ "multi-user.target" ];
     after = [ "docker.service" ];
     requires = [ "docker.service" ];
+    script = ''
+      IP=$(${pkgs.curl}/bin/curl -s https://ident.me)
+      ${pkgs.docker}/bin/docker swarm init --advertise-addr=$IP || true
+    '';
     serviceConfig = {
-      ExecStart = "${pkgs.docker-compose}/bin/docker-compose -f ${composeFile} up --scale ${appName}=${scale}";
-      ExecStop= "${pkgs.docker-compose}/bin/docker-compose -f ${composeFile} down";
-      Restart="always";
+      Type="oneshot";
+    };
+  };
+  systemd.services.stress_test = {
+    description = "Stress test docker compose";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "init-swarm.service" ];
+    requires = [ "docker.service" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.docker}/bin/docker stack deploy -c ${composeFile} stress_test";
       Type="simple";
-      #RemainAfterExit="yes";
     };
   };
 }
